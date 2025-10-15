eda17378b91ec47c8691b364d2cb14f7a16b9304
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-10-15 04:01:44 -0600

    Add project files.

---
+++ FluentTheme/Components/Pages/ThemeDetector.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+
+namespace FluentTheme.Components.Pages
+{
+    internal static class ThemeDetector
+    {
+        public static bool IsDarkModeEnabled()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                const string registryKey = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+                const string valueName = "AppsUseLightTheme";
+
+                object? registryValue = Registry.GetValue(registryKey, valueName, null);
+
+                if (registryValue is int themeValue)
+                {
+                    // 0 means Dark Mode, 1 means Light Mode
+                    return themeValue == 0;
+                }
+            }
+
+            // Default to Light Mode if the value is not found
+            return false;
+        }
+    }
+}
